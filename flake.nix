3459adb304be221ff8ef538a1dd41ba126683ff9
Ingo Ruhnke <grumbel@gmail.com>
2023-02-18 16:22:58 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    v4l2rtspserver_src.url = "git+https://github.com/mpromonet/v4l2rtspserver.git?submodules=1";
+    v4l2rtspserver_src.flake = false;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, v4l2rtspserver_src }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = rec {
+          default = v4l2rtspserver;
+
+          v4l2rtspserver = pkgs.stdenv.mkDerivation rec {
+            pname = "v4l2rtspserver";
+            version = "0.3.7-${v4l2rtspserver_src}.shortRev";
+
+            src = v4l2rtspserver_src;
+
+            nativeBuildInputs = with pkgs; [
+              pkgconfig
+              cmake
+              git
+            ];
+
+            buildInputs = with pkgs; [
+              alsa-lib
+              live555
+              log4cpp
+              openssl
+            ];
+          };
+        };
+      }
+    );
+}
